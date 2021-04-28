b719c9a0b618111c917c3fe91fbb52c29831fff1
ShakirovDamir <shakirov-damir@mail.ru>
2021-04-28 16:48:56 +0300

    28.04.2021

--- src/main/resources/static/deleteUser.js
+++ src/main/resources/static/deleteUser.js
@@ -1,11 +1,17 @@
 function deleteUser(id) {
-
     fetch('http://localhost:8080/delete/' + id, {
         method: 'DELETE',
         headers: {"Content-type": "application/json; charset=UTF-8"}
     })
         .then(response => {
-          showAllUsers();
-          showAllRoles();
+            let tBody = document.getElementById("tBody");
+            let rowIndex = 0;
+            for (let row of tBody.rows) {
+                if (row.cells[0].innerHTML === id.toString()) {
+                    break;
+                }
+                rowIndex++;
+            }
+            tBody.deleteRow(rowIndex);
         });
 }
--- src/main/resources/static/editUser.js
+++ src/main/resources/static/editUser.js
@@ -23,8 +23,32 @@ function editUser() {
 
     })
         .then(() => {
-            showAllUsers();
-            showAllRoles();
+            let tBody = document.getElementById("tBody");
+            let rowIndex = 0;
+            for (let row of tBody.rows) {
+                if (row.cells[0].innerHTML === user.id.toString()) {
+                    break;
+                }
+                rowIndex++;
+            }
+            let rowEdit = tBody.rows[rowIndex];
+            rowEdit.cells[0].innerHTML = user.id;
+            rowEdit.cells[1].innerHTML = user.name;
+            rowEdit.cells[2].innerHTML = user.lastName;
+            rowEdit.cells[3].innerHTML = user.age;
+            rowEdit.cells[4].innerHTML = user.email;
+
+            let cell5 = rowEdit.cells[5];
+            cell5.innerHTML = user.roles.join(' ');
+            let cell6 = rowEdit.cells[6];
+            cell6.innerHTML =
+                '<button type="button" onclick="getModalEdit(' + user.id +')" class="btn btn-primary btn-sm">\n' +
+                '   Edit</button>\n';
+
+            let cell7 = rowEdit.cells[7];
+            cell7.innerHTML =
+                '<button type="button" onclick="getModalDelete(' + user.id +')" class="btn btn-danger btn-sm">' +
+                'Delete</button>\n';
         });
 }
 
